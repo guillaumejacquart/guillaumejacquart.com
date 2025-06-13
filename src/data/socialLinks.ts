1f9b5d19806dd46ab026ce695fd4af2f29cd59ff
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-06-14 00:13:56 +0200

    Change order links

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -9,6 +9,12 @@ export interface SocialLink {
 
 export const socialLinks: SocialLink[] = [
   {
+    platform: 'LinkedIn',
+    icon: 'linkedin-logo',
+    url: 'https://www.linkedin.com/in/guillaume-jacquart-7157185a/',
+    description: 'I work at n8n'
+  },
+  {
     platform: 'GitHub',
     icon: 'github-logo',
     url: 'https://github.com/guillaumejacquart',
@@ -20,10 +26,4 @@ export const socialLinks: SocialLink[] = [
     url: 'https://guillaumejacquart.medium.com/',
     description: 'I write about stuff'
   },
-  {
-    platform: 'LinkedIn',
-    icon: 'linkedin-logo',
-    url: 'https://www.linkedin.com/in/guillaume-jacquart-7157185a/',
-    description: 'I work at n8n'
-  }
 ];
